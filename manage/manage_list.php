ea17bb2db46c69b7874b8f807eda295f36f894f6
Chaiwat Kaewmukdasawan <36642036+kchaiwat@users.noreply.github.com>
2018-04-22 23:44:48 +0700

    manage done

---
+++ manage/manage_list.php
@@ -0,0 +1,52 @@
+<?php include "../connect.php" ?>
+<?php session_start(); ?>
+<?php if($_SESSION["Username"]=='admin') { ?>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+  <!--
+  <th>ชื่อ</th>
+  <th>นามสกุล</th>
+  <th>ที่อยู่</th>
+  <th>อีเมล์</th>
+  <th>เบอร์โทรศัพท์</th>    -->
+
+<table border="1">
+<tr>
+    <th>รหัสสมาชิก</th>
+    <th>ชื่อสมาชิก</th>
+    <th>รหัสสินค้า</th>
+    <th>วันที่แก้ไข</th>
+</tr>
+
+<!-- **********************ดึงค่าของ order_head ***************************** -->
+<?php
+  $User_ID = $_SESSION["User_ID"];
+  $stmt = $pdo->prepare("SELECT * FROM manage LEFT JOIN user ON manage.User_ID = user.User_ID ");
+  $stmt->execute();
+  while ($row = $stmt->fetch()) {
+?>
+<tr>
+    <td><?=$row["User_ID"]?></td>
+    <td><?=$row["Username"]?></td>
+    <td><?=$row["Product_ID"]?></td>
+    <td><?=$row["Status_Date"]?></td>
+<tr>
+<?php }
+?>
+</table>
+<!-- ************************************* -->
+
+<a href="../index.php">ย้อนกลับ</a>
+</body>
+
+</html>
+<?php
+}
+else {
+  echo "กรุณาเข้าสู่ผู้ดูแลระบบ";
+  	//header("location: login.html");
+}
+?>
--- menu_admin.php
+++ menu_admin.php
@@ -1,3 +1,4 @@
 <br>
 <a href="Authentication\list_user.php">รายการสมาชิก</a><br>
 <a href="product\list_product.php">รายการสินค้า</a><br>
+<a href="manage\manage_list.php">ประวัติแก้ไขสินค้า</a><br>
