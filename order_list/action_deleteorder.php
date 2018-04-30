4c6da93c5b9789d6bc51dfc0bae070bb0fa76da4
Chaiwat <k.chaiwat@kkumail.com>
2018-04-30 13:44:03 +0700

    return qty and qty_sold done

--- order_list/action_deleteorder.php
+++ order_list/action_deleteorder.php
@@ -1,25 +1,33 @@
 <?php include "../connect.inc" ?>
+<?php include "../connect.php"?>
 <?php
+
+
 $Order_ID = $_GET['Order_ID'];
-$sql1 = "SELECT * FROM order_detail LEFT JOIN product ON order_detail.Product_ID = product.Product_ID where Order_ID=$Order_ID";
-$query1 = mysqli_query($conn, $sql1);
-$row = mysqli_fetch_array($query1);
-	$Product_ID = $row['Product_ID'];
-	$sql3	= "select * from product where Product_ID=$Product_ID";
-	$query3	= mysqli_query($conn, $sql3);
-	$row3	= mysqli_fetch_array($query3);
-					$count2 = mysqli_num_rows($query3);
-								for($i=0; $i<$count2; $i++){
-									$Product_ID = $row["Product_ID"];
-									$add_stock = $row["Product_stock"] + $row["Detail_qty"];
-									$add_sold = $row["Product_sold"] - $row["Detail_qty"];
-									$sql2	= "UPDATE product set Product_stock=$add_stock,Product_sold=$add_sold where Product_ID=$Product_ID";
-									$query2	= mysqli_query($conn, $sql2);
-								}
+
+/* คืนจำนวนสินค้า */
+	$stmt = $pdo->prepare("SELECT * FROM order_detail LEFT JOIN product ON order_detail.Product_ID = product.Product_ID where Order_ID=$Order_ID ");
+	$stmt->execute();
+	$Product_ID_array = array();
+	$add_stock = array();
+	$add_sold = array();
+	$count=0;
+	while ($row = $stmt->fetch()) {
+	$Product_ID_array[] = $row['Product_ID'];
+	$add_stock[] = $row['Product_stock'] + $row['Detail_qty'];
+	$add_sold[] = $row['Product_sold'] - $row['Detail_qty'];
+	$count = $count+1;
+ }
+ for($i=0;$i<$count;$i++){
+	$sql2	= "UPDATE product set Product_stock=$add_stock[$i],Product_sold=$add_sold[$i] where Product_ID=$Product_ID_array[$i]";
+	$query2	= mysqli_query($conn, $sql2);
+ }
+ /* ลบ */
 $sql	= "DELETE `order_detail`,`order_head` FROM `order_detail` INNER JOIN `order_head` WHERE `order_detail`.`Order_ID` = `order_head`.`Order_ID` AND `order_detail`.`Order_ID` = $Order_ID";
 $query	= mysqli_query($conn, $sql);
 if($query){
 	header("location: order_head_list.php");
 }
+
 
 ?>
